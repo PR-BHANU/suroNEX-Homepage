6c7850c28a968a2851baad0ddb94cd849c223852
Bhanu Prakash <prbhanu2007@gmail.com>
2025-12-16 22:11:19 +0530

    Added Navigation Bar

---
+++ app/components/NavigationBar.tsx
@@ -0,0 +1,40 @@
+export default function NavigationBar() {
+  return (
+    <nav className="nav">
+      <img
+        src="/suroNex-Text-Logo.svg"
+        alt="Logo"
+        className="logo"
+        aria-haspopup="true"
+      />
+      <div className="dropdown">
+        <button className="dropdown-button">Products</button>
+        <div className="dropdown-menu">
+          <a href="#">Cloud Security</a>
+          <a href="#">Inventory Management</a>
+          <a href="#">Governance, Risk & Compliance</a>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropdown-button">Company</button>
+        <div className="dropdown-menu">
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropdown-button">Resources</button>
+        <div className="dropdown-menu">
+          <a href="#">FAQs</a>
+          <a href="#">Whitepaper</a>
+        </div>
+      </div>
+
+      <a href="#" className="suron-AI">
+        Suron AI
+      </a>
+
+      <button className="demo-button">Book a Demo</button>
+    </nav>
+  );
+}
