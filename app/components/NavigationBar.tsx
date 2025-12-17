579328d7358ce4a39da9683501386ccadde23d4c
Bhanu Prakash <prbhanu2007@gmail.com>
2025-12-17 23:49:31 +0530

    Finished Navbar and made it mobile friendly

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,39 +1,39 @@
 export default function NavigationBar() {
   return (
     <nav className="nav">
-      <img
-        src="/suroNex-Text-Logo.svg"
-        alt="Logo"
-        className="logo"
-        aria-haspopup="true"
-      />
-      <div className="dropdown">
-        <button className="dropdown-button">Products</button>
-        <div className="dropdown-menu">
-          <a href="#">Cloud Security</a>
-          <a href="#">Inventory Management</a>
-          <a href="#">Governance, Risk & Compliance</a>
-        </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropdown-button">Company</button>
-        <div className="dropdown-menu">
-          <a href="#">About Us</a>
-          <a href="#">Contact Us</a>
-        </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropdown-button">Resources</button>
-        <div className="dropdown-menu">
-          <a href="#">FAQs</a>
-          <a href="#">Whitepaper</a>
-        </div>
-      </div>
+      <div className="nav-left">
+        <img src="/suroNex-Text-Logo.svg" alt="Logo" className="logo" />
 
-      <a href="#" className="suron-AI">
-        Suron AI
-      </a>
+        <div className="dropdown">
+          <button className="dropdown-button">Products</button>
+          <div className="dropdown-menu">
+            <a href="#">Cloud Security</a>
+            <a href="#">Inventory Management</a>
+            <a href="#">Governance, Risk & Compliance</a>
+          </div>
+        </div>
 
+        <div className="dropdown">
+          <button className="dropdown-button">Company</button>
+          <div className="dropdown-menu">
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button className="dropdown-button">Resources</button>
+          <div className="dropdown-menu">
+            <a href="#">FAQs</a>
+            <a href="#">Whitepaper</a>
+          </div>
+        </div>
+
+        <a href="#" className="suron-AI">
+          Suron AI
+        </a>
+      </div>
+      <br></br>
       <button className="demo-button">Book a Demo</button>
     </nav>
   );
